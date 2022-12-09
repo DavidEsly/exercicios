9e0e87b36d1ce55dc0690689e6af05e2bac0a285
DavidEsly <david.esly@hotmail.com>
2022-12-08 22:37:31 -0300

    Adicionado comentários

--- exercicio02/jscript.js
+++ exercicio02/jscript.js
@@ -1,9 +1,11 @@
 var xhr = new XMLHttpRequest();
 var doc;
+//função onde caso a resposta seja positiva ele faz a coleta dos dados e joga na variável doc
 xhr.onreadystatechange = function (){
     if (xhr.readyState == 4 && xhr.status == 200){
         doc = xhr.response;
         console.log(doc.entries)
+        //percorrendo o array de objetos
         doc.entries.forEach((entries, index) => {
             console.log('API: ' + entries["API"] + ' Category: ' + entries["Category"]);
         });
@@ -13,9 +15,9 @@ xhr.onreadystatechange = function (){
     
 }
 
+//pegando da url somente quem possuir a categoria animals
+xhr.open("GET", "https://api.publicapis.org/entries?category=animals");
 
-xhr.open("GET", "https://api.publicapis.org/entries");
+xhr.responseType = 'json'; //definindo o tipo de resposta recebido
 
-xhr.responseType = 'json';
-
-xhr.send();
+xhr.send(); //enviando a requisição
